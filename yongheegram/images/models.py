54eec507d939520c9e3aea2fb0d6418f8cc6b88b
yhhan <shiw122333@gmail.com>
2019-02-22 00:33:57 +0900

    add Images models and admin config + django rest framework using

--- yongheegram/images/models.py
+++ yongheegram/images/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from yongheegram.users import models as user_model
 
-# Create your models here.
+
+class TiemStampModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TiemStampModel):
+
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+
+class Comment(TiemStampModel):
+
+    """ Commend Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.message
+
+
+class Like(TiemStampModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'User:{} - Image Caption:{}'.format(self.creator.username, self.image.caption)
--- yongheegram/users/models.py
+++ yongheegram/users/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, URLField, TextField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+
+    """ User Model """
 
     GENDER_CHOICES = (
         ('male', 'Male'),
@@ -14,11 +16,13 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    website = URLField(null=True)
-    bio = TextField(null=True)
-    phone = CharField(max_length=140, null=True)
-    gender = CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def __str__(self):
         return self.username
