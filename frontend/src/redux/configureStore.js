ae6534597a3198fc505cda010d913e6ec59bf010
yhhan <shiw122333@gmail.com>
2019-03-06 00:19:39 +0900

    react-router, redux, middleware, import path likes module

---
+++ frontend/src/redux/configureStore.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { routerMiddleware, connectRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import users from "redux/modules/users";
+
+const env = process.env.NODE_ENV;
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)];
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const reducer = combineReducers({
+  users,
+  router: connectRouter(history)
+});
+
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
+
+export { history };
+
+export default store();
